a857b27a6dd36e3e61ccf8824cc57932f25f707c
DaveThePianoDude <dah_7@yahoo.com>
2013-12-09 20:26:22 -0500

    Try templates!

--- php-login-master/0-one-file/index.php
+++ php-login-master/0-one-file/index.php
@@ -356,6 +356,11 @@ class Login
         }
 
         echo 'Hello ' . $_SESSION['user_name'] . ', you are logged in.<br/><br/>';
+		
+		$main = new Template('choir.php', array('title' => 'Vienna Presbyterian Sanctuary Church Choir'));
+		
+		$main->render();
+		
         echo '<a href="' . $_SERVER['SCRIPT_NAME'] . '?action=logout">Log out</a>';
     }
 
