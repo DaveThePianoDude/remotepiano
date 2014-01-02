30185876d4e7c118b5fc30ec8e18785af3b4366f
DaveThePianoDude <dah_7@yahoo.com>
2014-01-01 20:10:59 -0500

    clean and clarify

--- public/index.php
+++ public/index.php
@@ -9,7 +9,6 @@ checkLoggedIn (Page::INDEX);
 <html>
    <body>
       <form name="logout" method="post" action="login.php">
-		<h3>This is the index page.</h3>
          <input type="hidden" name="action" value="logout" />
          <input type="submit" value="Logout" />
       </form>
