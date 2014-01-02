3ec816f5ef290c5c80189e226b4d27d66a423c75
DaveThePianoDude <dah_7@yahoo.com>
2014-01-01 20:47:51 -0500

    didit

--- public/functions.php
+++ public/functions.php
@@ -119,7 +119,7 @@ function checkLoggedIn($page)
  
 				$loginDiv = '<div id="login-box" class="error">Got here.</div>';
 				
-                header ('Location: index.php');
+                header ('Location: login.php');
  
                exit;
 
